29e8c8de6ed2c5c96d1ae02cafe7430aef2e8cb2
XumiSan <daffahidayatulah123@gmail.com>
2023-10-12 10:15:14 +0700

    Update Facing

--- PaidPort/Assets/Script/Gameplay/PlayerMovement.cs
+++ PaidPort/Assets/Script/Gameplay/PlayerMovement.cs
@@ -10,7 +10,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private Rigidbody2D rb;
     Vector2 movement;
-   
+    
 
     // Update is called once per frame
     void Update()
@@ -23,7 +23,33 @@ public class PlayerMovement : MonoBehaviour
     }
     private void Movement()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+        Vector2 direction = new Vector2 (x, 0);
+
+        if (x < 0)
+        {
+            Facing(false);
+        }
+        if (x > 0)
+        {
+            Facing(true);
+        }
+    }
+
+    private void Facing(bool isFacingRight)
+    {
+        if (isFacingRight == true)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            return;
+        }
+        if (isFacingRight == false)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            return;
+        }
+
     }
 }
