5637f6dd9a97edcf590bc9625ae7dac79d4950c7
XumiSan <daffahidayatulah123@gmail.com>
2023-11-22 12:02:54 +0700

    AddAMainMenu
    Add Main Menu and Fungsion Background Change Color

---
+++ PaidPort/Assets/Script/Gameplay/BackgroundColor.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundColor : MonoBehaviour
+
+{
+    public Transform player;
+    public Color brownColor = new Color(0.6f, 0.3f, 0.1f, 1f);
+
+    private Camera backgroundCamera;
+    private Color originalColor;
+
+    void Start()
+    {
+        
+        backgroundCamera = GetComponent<Camera>();
+
+        
+        originalColor = backgroundCamera.backgroundColor;
+    }
+
+    void Update()
+    {
+       
+        if (player.position.y <= -1f)
+        {
+           
+            ChangeBackgroundColor(brownColor);
+        }
+        else
+        {
+            
+            ChangeBackgroundColor(originalColor);
+        }
+    }
+
+    
+    void ChangeBackgroundColor(Color color)
+    {
+        backgroundCamera.backgroundColor = color;
+    }
+}
+
+
