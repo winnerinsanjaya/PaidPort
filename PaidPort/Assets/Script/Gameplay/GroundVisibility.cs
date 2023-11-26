e9c2bdcf3d23b8e9cc1f1ee34765a1a2f0bb4ec3
XumiSan <daffahidayatulah123@gmail.com>
2023-11-26 15:29:49 +0700

    AddBlockVision
    Fog Of War

---
+++ PaidPort/Assets/Script/Gameplay/GroundVisibility.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundVisibility : MonoBehaviour
+{
+    public SpriteRenderer groundSpriteRenderer;
+    public Sprite originalSprite;
+    public Sprite blackSprite;
+    public Sprite overlaySprite;
+
+    private bool isWithinVision = true;
+    private Collider2D playerCollider;
+    public float visionRange = 5f;
+    public float overlayRadius = 1.5f;
+
+    private float permanentSeenRadius = 0.1f;
+
+    private bool hasBeenSeen = false;
+    private bool permanentSeen = false;
+
+    private void Start()
+    {
+        playerCollider = GameObject.FindGameObjectWithTag("Player").GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+
+        float distanceToPlayer = Vector2.Distance(transform.position, playerCollider.transform.position);
+
+        if (!permanentSeen && (distanceToPlayer <= permanentSeenRadius || distanceToPlayer <= overlayRadius))
+        {
+            permanentSeen = true;
+        }
+
+        if (permanentSeen)
+        {
+
+            if (!hasBeenSeen)
+            {
+                hasBeenSeen = true;
+                groundSpriteRenderer.sprite = originalSprite;
+            }
+        }
+        else if (distanceToPlayer <= visionRange)
+        {
+
+            if (!isWithinVision)
+            {
+                groundSpriteRenderer.sprite = originalSprite;
+                isWithinVision = true;
+            }
+
+            if (distanceToPlayer <= (visionRange - overlayRadius))
+            {
+
+                groundSpriteRenderer.sprite = originalSprite;
+            }
+            else
+            {
+
+                groundSpriteRenderer.sprite = overlaySprite;
+            }
+        }
+        else
+        {
+
+            if (isWithinVision)
+            {
+                groundSpriteRenderer.sprite = blackSprite;
+                isWithinVision = false;
+            }
+        }
+    }
+}
